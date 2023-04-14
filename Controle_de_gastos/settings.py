5d3ad24b4f73f178b3fbb9ab5f2bd066be7d5945
AlanVitor <alanvictor100@hotmail.com>
2023-04-14 01:41:56 -0300

    Alterando o banco de dados para MySQL e instalando o MySQL

--- Controle_de_gastos/settings.py
+++ Controle_de_gastos/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'Controle_de_gastos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Engeseltsoftware_desafio',
+        'USER': 'alanvitor',
+        'PASSWORD': 'alanvitor',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -110,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
